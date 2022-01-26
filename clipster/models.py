2797ed897c6abc4ddd05078eef6558da33a7feab
MC51 <mc51@users.noreply.github.com>
2022-01-26 00:36:47 +0000

    add format field in django model for image sharing

--- clipster/models.py
+++ clipster/models.py
@@ -5,10 +5,11 @@ class Clip(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     text = models.TextField(max_length=10000)
+    format = models.CharField(max_length=3, blank=True, default="txt")
     device = models.CharField(max_length=100, blank=True, default="unspecified")
 
     def __str__(self):
-        return f"user: {self.user} text: {self.text} created at: {self.created_at} device: {self.device}"
+        return f"user: {self.user} format: {self.format} text: {self.text} created at: {self.created_at} device: {self.device}"
 
     class Meta:
         ordering = ("created_at",)
